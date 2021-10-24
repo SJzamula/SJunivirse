8fc077ac1fa2eb8bf2061a8d7c58484d93091946
Zamula-Liuba <zamula.l08l@gmail.com>
2021-10-24 14:41:13 +0300

    add login and registration screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,90 +1,148 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:sjuniverse/screens/registration_screen.dart';
 
 class LoginScreen extends StatefulWidget {
-  LoginScreen({Key? key}) : super(key: key);
+  const LoginScreen({Key? key}) : super(key: key);
 
   @override
   _LoginScreenState createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-
-  // form key
   final _formKey = GlobalKey<FormState>();
 
-  // editing controller
-  final TextEditingController emailController = new TextEditingController();
-  final TextEditingController passwordController = new TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+
+  final TextEditingController passwordController = TextEditingController();
 
 
   @override
   Widget build(BuildContext context) {
-
-    // email field
-
     final emailField = TextFormField(
       autofocus: false,
       controller: emailController,
       keyboardType: TextInputType.emailAddress,
-      // validator: () {},
-      onSaved: (value)
-      {
+      validator: (value) {
+        if (value!.isEmpty) {
+          return ("Please Enter Your Email");
+        }
+        if (!RegExp(
+            r"""^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+""")
+            .hasMatch(value)) {
+          return ("Please enter a valid email");
+        }
+        return null;
+      },
+      onSaved: (value) {
         emailController.text = value!;
       },
       textInputAction: TextInputAction.next,
       decoration: InputDecoration(
-        prefixIcon: Icon(Icons.mail),
-        contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
-        hintText: "Email",
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(18),
-        )
-      ),
+          prefixIcon: Icon(Icons.mail),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Email",
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          )),
     );
 
-
-
-    // password field
     final passwordField = TextFormField(
       autofocus: false,
       controller: passwordController,
-      // validator: () {},
-      onSaved: (value)
-      {
+      obscureText: true,
+
+      validator: (value){
+        RegExp regex = RegExp(r'^.{8,}$');
+        if (value!.isEmpty) {
+          return ("Password is required for login");
+        }
+        {
+          if (!regex.hasMatch(value))
+          {
+            return ("Please Enter Valid Password(Min. 6 Character");
+          }
+        }
+      },
+      onSaved: (value) {
         passwordController.text = value!;
       },
       textInputAction: TextInputAction.done,
       decoration: InputDecoration(
-          prefixIcon: Icon(Icons.vpn_key),
+          prefixIcon: Icon(Icons.https),
           contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
           hintText: "Password",
           border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(18),
-          )
-      ),
+            borderRadius: BorderRadius.circular(10),
+          )),
     );
 
+    final loginButton = Material(
+        elevation: 5,
+        borderRadius: BorderRadius.circular(30),
+        child: MaterialButton(
+            color: Colors.redAccent,
+            padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+            minWidth: MediaQuery.of(context).size.width,
+            onPressed: () {
+              //signIn(emailController.text, passwordController.text);
+            },
+            child: Text(
+              "Login",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            )));
+
+    final buildSingUp =
+    Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+      Text("Don`t have an acсount? "),
+      GestureDetector(
+          onTap: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => RegistrationScreen()));
+          },
+          child: Text(
+            "SignUp",
+            style: TextStyle(fontWeight: FontWeight.w600, fontSize: 15),
+          )),
+    ]);
 
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: SingleChildScrollView(
-          child: (
-          Container(
-            color: Colors.white,
-            child: Form(
-              key: _formKey,
-              child: Column(
-                children: <Widget>[
-                  emailField,
-                  passwordField,
-                ],
-              ),
-            ),
-          )
-          ),
-        ),
-      ),
-    );
+        backgroundColor: Colors.white,
+        body: Center(
+          child: SingleChildScrollView(
+              child: Container(
+                  color: Colors.white,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Form(
+                        key: _formKey,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: SizedBox(
+                                  height: 150,
+                                  child: Image.asset("assets/galaxy.jpg",
+                                      fit: BoxFit.contain)),
+                            ),
+                            SizedBox(height: 45),
+                            emailField,
+                            SizedBox(height: 25),
+                            passwordField,
+                            SizedBox(height: 35),
+                            loginButton,
+                            SizedBox(height: 15),
+                            buildSingUp
+                          ],
+                        )),
+                  ))),
+        ));
   }
+
 }
---
+++ lib/screens/registration_screen.dart
@@ -0,0 +1,229 @@
+import 'package:flutter/material.dart';
+
+class RegistrationScreen extends StatefulWidget {
+  const RegistrationScreen({Key? key}) : super(key: key);
+
+  @override
+  _RegistrationScreenState createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final firstNameEditingController = TextEditingController();
+  final secondNameEditingController = TextEditingController();
+  final emailNameEditingController = TextEditingController();
+  final passwordNameEditingController = TextEditingController();
+  final confirmPasswordNameEditingController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+
+    final signUpButton = Material(
+      elevation: 5,
+      borderRadius: BorderRadius.circular(30),
+      color: Colors.redAccent,
+      child: MaterialButton(
+          padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          minWidth: MediaQuery
+              .of(context)
+              .size
+              .width,
+          onPressed: () {
+            //Navigator.of(context)
+            //singUp(emailNameEditingController.text, passwordNameEditingController.text);
+
+          },
+          child: Text(
+            "SignUp",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),
+          )),);
+
+
+    final firstNameField = TextFormField(
+      autofocus: false,
+      controller: firstNameEditingController,
+      keyboardType: TextInputType.emailAddress,
+      validator: (value) {
+        RegExp regex = RegExp(r'^.{2,}$');
+        if (value!.isEmpty) {
+          return ("First name cannot be Empty");
+        }
+        {
+          if (!regex.hasMatch(value)) {
+            return ("Enter Valid name(Min. 2 Character");
+          }
+        }
+        return null;
+      },
+      onSaved: (value) {
+        firstNameEditingController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+          prefixIcon: Icon(Icons.account_circle),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "First Name",
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          )),
+    );
+
+    final secondNameField = TextFormField(
+      autofocus: false,
+      controller: secondNameEditingController,
+      keyboardType: TextInputType.emailAddress,
+      validator: (value) {
+        if (value!.isEmpty) {
+          return ("First name cannot be Empty");
+        }
+      },
+      onSaved: (value) {
+        secondNameEditingController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+          prefixIcon: Icon(Icons.account_circle),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Second Name",
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          )),
+    );
+
+    final emailField = TextFormField(
+      autofocus: false,
+      controller: emailNameEditingController,
+      keyboardType: TextInputType.emailAddress,
+      validator: (value) {
+        if (value!.isEmpty) {
+          return ("Please Enter Your Email");
+        }
+        if (!RegExp(
+            r"""^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+""")
+            .hasMatch(value)) {
+          return ("Please enter a valid email");
+        }
+        return null;
+      },
+      onSaved: (value) {
+        emailNameEditingController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+          prefixIcon: Icon(Icons.mail),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Email",
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          )),
+    );
+
+    final passwordField = TextFormField(
+      autofocus: false,
+      controller: passwordNameEditingController,
+      obscureText: true,
+      keyboardType: TextInputType.emailAddress,
+      validator: (value) {
+        RegExp regex = RegExp(r'^.{8,}$');
+        if (value!.isEmpty) {
+          return ("Password is required for login");
+        }
+        {
+          if (!regex.hasMatch(value)) {
+            return ("Please Enter Valid Password(Min. 6 Character");
+          }
+        }
+      },
+      onSaved: (value) {
+        passwordNameEditingController.text = value!;
+      },
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+          prefixIcon: Icon(Icons.https),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Password",
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          )),
+    );
+
+    final confirmPasswordField = TextFormField(
+      autofocus: false,
+      controller: confirmPasswordNameEditingController,
+      obscureText: true,
+
+
+      validator: (value) {
+        if (confirmPasswordNameEditingController.text!= passwordNameEditingController.text){
+          return "Password don`t match";
+        }
+        return null;
+      },
+
+      onSaved: (value) {
+        confirmPasswordNameEditingController.text = value!;
+      },
+      textInputAction: TextInputAction.done,
+      decoration: InputDecoration(
+          prefixIcon: Icon(Icons.https),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Confirm Password",
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          )),
+    );
+
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back, color: Colors.red,),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ),
+        body: Center(
+          child: SingleChildScrollView(
+              child: Container(
+                  color: Colors.white,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Form(
+                        key: _formKey,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: SizedBox(
+                                  height: 150,
+                                  child: Image.asset("assets/images/galaxy.jpg",
+                                      fit: BoxFit.contain)),
+                            ),
+                            SizedBox(height: 25),
+                            firstNameField,
+                            SizedBox(height: 15),
+                            secondNameField,
+                            SizedBox(height: 15),
+                            emailField,
+                            SizedBox(height: 15),
+                            passwordField,
+                            SizedBox(height: 15),
+                            confirmPasswordField,
+                            SizedBox(height: 15),
+                            signUpButton,
+                            SizedBox(height: 15),
+                          ],
+                        )),
+                  ))),
+        ));
+  }
+
+}
